6c117537acef8ee42516fdb3c523b9b50a1566c5
Minna <liu_minna@hotmail.com>
2023-05-01 13:28:57 +0200

    first commit

---
+++ app/components/Container.tsx
@@ -0,0 +1,15 @@
+interface ContainerProps {
+    children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({
+    children
+}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
+
+export default Container;
---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import Container from "../Container";
+
+const Navbar = () => {
+    return (
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div className="
+    py-4
+    border-b-[1px]">
+                <Container>
+what is going on
+                </Container>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
