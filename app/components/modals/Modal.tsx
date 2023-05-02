d1af16ec23be3017025f8c867e97afe3798707eb
Minna <liu_minna@hotmail.com>
2023-05-02 16:38:25 +0200

    updates modal with icon

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useCallback, useEffect, useState } from 'react';
+import {IoMdClose} from 'react-icons/io'
 
 interface ModalProps {
     isOpen?: boolean;
@@ -52,10 +53,96 @@ const Modal: React.FC<ModalProps> = (
         onSubmit();
     }, [disabled, onSubmit]);
 
-    return (
-        <div>
+    const handleSecondaryAction = useCallback(() => {
+        if (disabled || !secondaryAction) {
+            return;
+        }
+        secondaryAction();
+    }, [disabled, secondaryAction])
 
-        </div>);
+    if (!isOpen) {
+        return null;
+    }
+
+    return (
+        <>
+            <div
+                className='
+            justify-center
+            items-center
+            flex
+            overflow-x-hidden
+            overflow-y-auto
+            fixed
+            inset-0
+            z-50
+            outlin-none
+            focus:outline-none
+            bg-neutral-800/70'>
+                <div
+                    className='
+                relative
+                w-full
+                md:w-4/6
+                lg:w-3/6
+                xl:w-2/5
+                my-6
+                mx-auto
+                h-full
+                lg:h-auto
+                md:h-auto
+                '>
+                    {/* CONTENT */}
+                    <div
+                        className={`
+                            translate
+                            duration-300
+                            h-full
+                            ${showModal ? 'translate-y-0' : 'translate-y-full'}
+                            ${showModal ? 'opacity-100' : 'opacity-0'}
+                        `}>
+                        <div className='
+                            translate
+                            h-full
+                            lg:h-auto
+                            md:h-auto
+                            border-0
+                            rounded-lg
+                            shadow-lg
+                            relative
+                            flex
+                            flex-col
+                            w-full
+                            bg-white
+                            outline-none
+                            focus:outline-none'
+                        >
+                            {/* HEADER */}
+                            <div
+                                className='
+                                flex
+                                items-center
+                                p-6
+                                rounded-t
+                                justify-center
+                                relative
+                                border-b-[1px]'>
+                                <button
+                                    className="
+                                    p-1
+                                    border-0
+                                    hover:opacity-70
+                                    transition
+                                    absolute
+                                    left-9">
+                                        <IoMdClose size={18} />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </>);
 }
 
 export default Modal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal />
+          <Modal
+          isOpen={true} />
           <Navbar />
         </ClientOnly>
         {children}
