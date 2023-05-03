3c77907bd0b22d1907586c3ed206321eb6cbc785
Minna <liu_minna@hotmail.com>
2023-05-03 12:58:43 +0200

    updates the button in modal

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+
 import { IconType } from "react-icons";
 
 interface ButtonProps {
@@ -17,11 +20,14 @@ const Button: React.FC<ButtonProps> = (
         disabled,
         outline,
         small,
-        icon
+        icon: Icon
     }
 ) => {
     return (
-        <button className={`
+        <button 
+        onClick={onClick}
+        disabled={disabled}
+        className={`
        relative
        disabled:opacity-70
        disabled:cursor-not-allowed
@@ -29,10 +35,24 @@ const Button: React.FC<ButtonProps> = (
        hover:opacity-80
        transition
        w-full
-       ${outline? 'bg-white': 'bg-rose-500'}
-       ${outline? 'bg-white': 'bg-rose-500'}
+       ${outline ? 'bg-white' : 'bg-rose-500'}
+        ${outline ? 'border-black' : 'border-rose-500'}
+        ${outline ? 'text-black' : 'text-white'}
+        ${small ? 'py-1' : 'py-3'}
+        ${small ? 'text-sm' : 'text-md'}
+        ${small ? 'font-light' : 'font-semibold'}
+        ${small ? 'border-[1px]' : 'border-2'}
         `}
-    >
+        >
+            {Icon &&
+                (
+                    <Icon size={24}
+                        className="
+                absolute
+                left-4
+                top-3
+                " />
+                )}
             {label}
         </button>
     )
