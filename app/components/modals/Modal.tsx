eebb6926a5deefeda1c5b417e1f9544c511081a0
Minna <liu_minna@hotmail.com>
2023-05-03 13:57:51 +0200

    installed axios and react-hook-form

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -14,7 +14,7 @@ interface ModalProps {
     actionLabel: string;
     disabled?: boolean;
     secondaryAction?: () => void;
-    secondaryLabel?: string;
+    secondaryActionLabel?: string;
 }
 
 const Modal: React.FC<ModalProps> = (
@@ -28,7 +28,7 @@ const Modal: React.FC<ModalProps> = (
         actionLabel,
         disabled,
         secondaryAction,
-        secondaryLabel
+        secondaryActionLabel
     }
 ) => {
     const [showModal, setShowModal] = useState(isOpen);
@@ -155,8 +155,18 @@ const Modal: React.FC<ModalProps> = (
                                 items-center
                                 gap-4
                                 w-full'>
-                                    <Button 
-                                    label="My Button"/>
+                                    {secondaryAction && secondaryActionLabel && (
+                                        <Button
+                                            outline
+                                            disabled={disabled}
+                                            label={secondaryActionLabel}
+                                            onClick={handleSecondaryAction}
+                                        />)}
+                                    <Button
+                                        disabled={disabled}
+                                        label={actionLabel}
+                                        onClick={handleSubmit}
+                                    />
                                 </div>
 
                             </div>
