b8f513864972aebedcc3d737eda6372144fe819a
Minna <liu_minna@hotmail.com>
2023-05-04 15:44:19 +0200

    finishes input component

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-interface HeadingProps{
+interface HeadingProps {
   title: string;
-  subtitle ?: string;
-  center ?: boolean;
+  subtitle?: string;
+  center?: boolean;
 }
 
 const Heading: React.FC<HeadingProps> = ({
@@ -12,13 +12,13 @@ const Heading: React.FC<HeadingProps> = ({
   center
 }) => {
   return (
-    <div className={center ? 'text:center' : 'text-start'}>
+    <div className={center ? 'text-center' : 'text-start'}>
       <div className="text-2xl font-bold">
         {title}
       </div>
       <div className="font-light text-neutral-500 mt-2">
         {subtitle}
-        </div>
+      </div>
     </div>
   )
 }
