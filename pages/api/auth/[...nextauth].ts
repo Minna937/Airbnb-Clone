46d152e5b13ff0b9e28567ad6effe155fefc9369
Minna <liu_minna@hotmail.com>
2023-05-08 15:14:05 +0200

    updates User type

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -25,7 +25,7 @@ export const authOptions: AuthOptions = {
                 email: { label: 'email', type: 'text' },
                 password: { label: 'password', type: 'password' },
             },
-
+      
             async authorize(credentials) {
                 if (!credentials?.email || !credentials?.password) {
                     throw new Error('Invalid credentials');
