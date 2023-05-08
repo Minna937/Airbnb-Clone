e29a96273ec53b00a126eff249c66953c1d3978f
Minna <liu_minna@hotmail.com>
2023-05-08 15:28:13 +0200

    Github login

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -16,6 +16,7 @@ import Heading from '../Heading';
 import Input from '../inputs/Inputs';
 import { toast } from 'react-hot-toast'
 import Button from "../Button";
+import { signIn } from "next-auth/react";
 
 
 const RegisterModal = () => {
@@ -95,7 +96,7 @@ const RegisterModal = () => {
                 outline
                 label="Continue with Github"
                 icon={AiFillGithub}
-                onClick={() => { }} />
+                onClick={() => {signIn('github') }} />
             <div className="
                 text-neutral-500
                 text-center
