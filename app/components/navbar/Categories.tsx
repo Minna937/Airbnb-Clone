64f3bb1b9dea6597f00fabd1c6084a7b0f1a635c
Minna <liu_minna@hotmail.com>
2023-05-10 20:14:14 +0200

    updates category component

---
+++ app/components/navbar/Categories.tsx
@@ -0,0 +1,48 @@
+import Container from "../Container";
+import { TbBeach } from 'react-icons/tb';
+import { GiWindmill } from 'react-icons/gi';
+import { MdOutlineVilla } from 'react-icons/md';
+
+export const categories = [{
+    label: 'Beach',
+    icon: TbBeach,
+    description: 'This perperty is close to the beach!'
+},
+{
+    label: 'Windmills',
+    icon: GiWindmill,
+    description: 'This perperty has windmills!'
+},
+{
+    label: 'Modern',
+    icon: MdOutlineVilla,
+    description: 'This perperty is modern'
+},
+]
+
+const Categories = () => {
+    return (
+        <Container>
+            <div className="
+            pt-4
+            flex
+            flex-row
+            items-center
+            justify-between
+            overflow-x-auto">
+
+                {categories.map((item)=>{
+                    return <CategoryBox 
+                    key = {item.label}
+                    label = {item.label}
+                    description = {item.description}
+                    icon = {item.icon}
+                        />
+                })}
+            </div>
+
+        </Container>
+    )
+}
+
+export default Categories;
