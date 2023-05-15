97b09ec2cbc5ad9c4c53f6bd98b35a62ad0a8c72
Minna <liu_minna@hotmail.com>
2023-05-15 12:43:54 +0200

    updates STEPS

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -2,9 +2,29 @@
 
 import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
+import { useState } from "react";
+
+enum STEPS {
+    CATEGORY = 0,
+    LOCATION = 1,
+    INFO = 2,
+    IMAGES = 3,
+    DESCRIPTION = 4,
+    PRICE = 5
+}
 
 const RentModal = () => {
     const rentModal = useRentModal();
+
+    const [step, setStep] = useState(STEPS.CATEGORY);
+
+    const onBack = () => {
+        setStep((value) => value - 1);
+    };
+
+     const onNext = () => {
+        setStep((value) => value + 1);
+    };
 
     return (
         <Modal
