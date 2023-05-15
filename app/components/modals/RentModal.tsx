828d0e8c5fdf9a2e385c8289ce36721c79b19c54
Minna <liu_minna@hotmail.com>
2023-05-15 12:59:50 +0200

    updates categories in RentModal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -4,6 +4,7 @@ import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
 import { useMemo, useState } from "react";
 import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
 
 enum STEPS {
     CATEGORY = 0,
@@ -41,15 +42,27 @@ const RentModal = () => {
         return 'Back';
     }, [step]);
 
-let bodyContent = (
-    <div className="flex flex-col gap-8">
-        <Heading 
-        title = "Which of these best describes your place?"
-        subtitle="Pick a category"
-        />
-
-    </div>
-)
+    let bodyContent = (
+        <div className="flex flex-col gap-8">
+            <Heading
+                title="Which of these best describes your place?"
+                subtitle="Pick a category"
+            />
+            <div className="
+grid
+grid-cols-1
+md:grid-cols-2
+gap-3
+max-h-[50vh]
+overflow-y-auto">
+                {categories.map((item) => (
+                    <div key={item.label} className="col-span-1">
+                        {item.label}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 
 
     return (
