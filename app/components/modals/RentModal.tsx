2dcb6e67f5cdd59e364ac3635418099d82ec4c25
Minna <liu_minna@hotmail.com>
2023-05-17 15:25:54 +0200

    finishes counter

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -51,6 +51,8 @@ const RentModal = () => {
     const category = watch('category');
     const location = watch('location');
     const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount')
 
     const Map = useMemo(() => dynamic(() => import('../Map'), {
         ssr: false
@@ -143,6 +145,21 @@ const RentModal = () => {
                     title="Guests"
                     subtitle="How many guests do you allow?"
                     value={guestCount}
+                    onChange={(value) => setCustomValue('guestCount', value)}
+                />
+                <hr />
+                <Counter
+                    title="Rooms"
+                    subtitle="How many rooms do you have?"
+                    value={roomCount}
+                    onChange={(value) => setCustomValue('roomCount', value)}
+                />
+                <hr />
+                <Counter
+                    title="Bathrooms"
+                    subtitle="How many bathrooms do you have?"
+                    value={bathroomCount}
+                    onChange={(value) => setCustomValue('bathroomCount', value)}
                 />
             </div>
         )
