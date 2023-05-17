2a33ff10463adb752e6bab5a8fa5b7f96dfdd2e4
Minna <liu_minna@hotmail.com>
2023-05-17 14:15:07 +0200

    updates step INFO

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -50,10 +50,10 @@ const RentModal = () => {
     const category = watch('category');
     const location = watch('location');
 
-    const Map = useMemo(() => dynamic(() => import('../Map'), { 
-        ssr: false 
-      }), [location]);
-    
+    const Map = useMemo(() => dynamic(() => import('../Map'), {
+        ssr: false
+    }), [location]);
+
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -125,7 +125,18 @@ const RentModal = () => {
                 <CountrySelect
                     value={location}
                     onChange={(value) => setCustomValue('location', value)} />
-                  <Map center={location?.latlng} />
+                <Map center={location?.latlng} />
+            </div>
+        )
+    };
+
+    if (step === STEPS.INFO) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading 
+                title="Sahre some basics about your place"
+                subtitle="What amenities do you have?" 
+                />
             </div>
         )
     }
