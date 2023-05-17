2abceb5f7c7ab9d2be62107bb716bd446485c177
Minna <liu_minna@hotmail.com>
2023-05-17 11:46:56 +0200

    updates Map

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -6,7 +6,7 @@ import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/markerShadow.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 //@ts-ignore
 delete L.Icon.Default.prototype._getIconUrl;
@@ -27,10 +27,13 @@ const Map: React.FC<MapProps> = ({
         <MapContainer
             center={center as L.LatLngExpression || [51, -0.09]}
             zoom={center ? 4 : 2}
-            scrollWheelZoom = {false}
+            scrollWheelZoom={false}
             className="h-[35vh] rounded-lg"
         >
-
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
         </MapContainer>
     )
 };
--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -5,10 +5,10 @@ import Modal from "./Modal";
 import { useMemo, useState } from "react";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
-import Map from "../Map";
 import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
 import { FieldValues, useForm } from "react-hook-form";
+import dynamic from "next/dynamic";
 
 enum STEPS {
     CATEGORY = 0,
@@ -49,6 +49,10 @@ const RentModal = () => {
 
     const category = watch('category');
     const location = watch('location');
+
+    const Map = useMemo(() => dynamic(() => import('../Map'), { 
+        ssr: false 
+      }), [location]);
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -120,7 +124,9 @@ const RentModal = () => {
                 <CountrySelect
                 value={location} 
                 onChange={(value)=>setCustomValue('location',value)}/>
-                <Map />
+                <Map 
+                center={location?.laglng}
+                />
             </div>
         )
     }
