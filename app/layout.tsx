8796dfaa0617b93ae5f95ea98b7d2a29e67a6f3b
Minna <liu_minna@hotmail.com>
2023-05-19 11:51:04 +0200

    updates listing parent

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,9 @@ export default async function RootLayout({
           <RegisterModal />
           <Navbar currentUser={currentUser}/>
         </ClientOnly>
+        <div className="pb-20 pt-28">
         {children}
+        </div>
       </body>
     </html>
   )
