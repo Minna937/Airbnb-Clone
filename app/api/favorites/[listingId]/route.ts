ea18dcb6bf32b6a831bc16470364721a82f45d34
Minna <liu_minna@hotmail.com>
2023-05-22 17:02:22 +0200

    updates route for favoriteIds

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -37,4 +37,36 @@ export async function POST(
     });
 
     return NextResponse.json(user);
+};
+
+export async function DELETE(
+    request: Request,
+    { params }: { params: Iparams }
+) {
+
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+        return NextResponse.error();
+    }
+
+    const { listingId } = params;
+
+    if (!listingId || typeof listingId !== 'string') {
+        throw new Error('Invalid ID');
+    }
+
+    let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+    favoriteIds = favoriteIds.filter((id) => { id !== listingId }); //remove id which matches listingId;
+
+    const user = await prisma.user.update({
+        where: {
+            id: currentUser.id
+        },
+        data: {
+            favoriteIds: favoriteIds
+        }
+    })
+    return NextResponse.json(user);
 }
