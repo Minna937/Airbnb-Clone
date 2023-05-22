6aad6ca1abe396392b786f05c6af7320b03c144e
Minna <liu_minna@hotmail.com>
2023-05-22 14:54:09 +0200

    updates listing square

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -5,6 +5,8 @@ import { SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import { useCallback, useMemo } from "react";
+import { format } from 'date-fns';
+import Image from "next/image";
 
 interface ListingCardProps {
     data: Listing;
@@ -48,20 +50,44 @@ const ListingCard: React.FC<ListingCardProps> = ({
         return data.price;
     }, [reservation, data.price]);
 
-    const reservationDate = useMemo((
-
-    ) => {
+    const reservationDate = useMemo(() => {
         if (!reservation) {
             return null;
         }
 
         const start = new Date(reservation.startDate);
         const end = new Date(reservation.endDate);
-    }, [])
+        return `${format(start, 'PP')} - ${format(end, 'PP')}`
+    }, [reservation]);
 
 
     return (
-        <div></div>
+        <div
+            onClick={() => { router.push(`/listings/${data.id}`) }}
+            className="col-span-1 cursor-pointer group"
+        >
+            <div className="flex flex-col gap-2 w-full">
+                <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+                    <Image
+                        fill
+                        alt="Listing"
+                        src={data.imageSrc}
+                        className="
+                    object-cover
+                    h-full
+                    w-full
+                    group-hover:scale-110
+                    transition
+                    "
+                    />
+                    <div className="absolute top-3 right-3">
+                        <HeartButton
+                            listingId={data.id}
+                            currentUser={currentUser} />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 };
 
