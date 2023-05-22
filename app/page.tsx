57da260742034639c496b4c9e94dcf5e06734f7c
Minna <liu_minna@hotmail.com>
2023-05-22 13:26:04 +0200

    updates icon from dollar to euro

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import getListings from "./actions/getListings";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
 export default async function Home() {
   const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return (
@@ -29,7 +32,8 @@ export default async function Home() {
         ">
           {listings.map((listing: any) => {
             return (
-             <ListingCard 
+             <ListingCard
+             currentUser = {currentUser}
              key={listing.id}
              data={listing}
              />
