1e39704de862b4a61bce1ce2105ac798924724d3
Minna <liu_minna@hotmail.com>
2023-05-23 15:54:21 +0200

    updates ListingInfo

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -5,6 +5,11 @@ import { SafeUser } from "@/app/types";
 import { IconType } from "react-icons";
 import Avatar from "../Avatar";
 import ListingCategory from "./ListingCategory";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(()=>import('../Map'),{
+    ssr:false
+});
 
 interface ListingInfoProps {
     user: SafeUser;
@@ -74,6 +79,13 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
                     description={category.description}
                 />
             )}
+            <hr />
+            <div className="text-lg font-light text-neutral-500">
+                {description}
+            </div>
+            <hr />
+            <Map center ={coordinates} />
+
         </div>
     )
 }
