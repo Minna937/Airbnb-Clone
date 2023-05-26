8c36407ad57dda9fc3d860323dab108a158ccad1
Minna <liu_minna@hotmail.com>
2023-05-26 10:44:17 +0200

    updates router after reservation

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -69,8 +69,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
             .then(() => {
                 toast.success('Listing reserved!');
                 setDateRange(initialDateRange);
-                //Redirect to /trips
-                router.refresh()
+                router.push('/trips');
             })
             .catch((error) => {
                 toast.error('Something went wrong.')
