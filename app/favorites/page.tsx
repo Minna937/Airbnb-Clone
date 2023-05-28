34d2ab86fb8a9ed88b567f3fc358cfff25e92063
Minna <liu_minna@hotmail.com>
2023-05-28 11:40:39 +0200

    updates getListings for the properties page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,15 +1,35 @@
 import EmptyState from "../components/EmptyState";
 import ClientOnly from "../components/ClientOnly";
+
 import getCurrentUser from "../actions/getCurrentUser";
+import getFavoriteListings from "../actions/getFavoriteListings";
+import FavoritesClient from "./FavoritesClient";
+import ListingCard from "../components/listings/ListingCard";
+import Container from "../components/Container";
 
 const ListingPage = async () => {
+    const listings = await getFavoriteListings();
+    const currentUser = await getCurrentUser();
+
+    if (listings.length === 0) {
+        return (
+            <ClientOnly>
+                <EmptyState
+                    title="No favorites found"
+                    subtitle="Looks like you have no favorite listings."
+                />
+            </ClientOnly>
+        )
+    }
+
     return (
         <ClientOnly>
-            <EmptyState
-                title="No favorites found"
-                subtitle="Looks like you have no favorite listings."
+            <FavoritesClient
+                listings={listings}
+                currentUser={currentUser}
             />
         </ClientOnly>
+
     )
 }
 
