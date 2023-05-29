698e6d86712c036d43aa0362fbe8510d0c1135ce
Minna <liu_minna@hotmail.com>
2023-05-29 11:18:48 +0200

    updates SearchModal with enum and Map import

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -1,10 +1,40 @@
 'use client';
 
 import useSearchModal from "@/app/hooks/useSearchModal";
-import Modal from "./Modal"
+import { useState,useMemo } from "react";
+import { Range } from "react-date-range";
+
+import Modal from "./Modal";
+import dynamic from "next/dynamic";
+
+import { useParams, useRouter, useSearchParams } from "next/navigation";
+
+enum STEPS {
+    LOCATION = 0,
+    DATE = 1,
+    INFO = 2
+}
 
 const SearchModal = () => {
+    const router = useRouter();
+    const params = useSearchParams();
     const searchModal = useSearchModal();
+
+    const [step, setSetp] = useState(STEPS.LOCATION);
+    const [guestCount, setGuestCount] = useState(1);
+    const [roomCount, setRoomCount] = useState(1);
+    const [bathroomCount, setBathroomCount] = useState(1);
+    const [dateRange, setDateRange] = useState<Range>({
+        startDate: new Date(),
+        endDate: new Date(),
+        key: 'selection'
+    });
+
+    const Map = useMemo(() => dynamic(() => import('../Map'), {
+        ssr: false,
+    }),[location])
+
+
     return (
         <Modal
             isOpen={searchModal.isOpen}
